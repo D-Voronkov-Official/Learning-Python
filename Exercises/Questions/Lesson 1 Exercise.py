81afe323a8ba03126e7fda64da6df5014e8e751d
D-Voronkov-Official <164271233+D-Voronkov-Official@users.noreply.github.com>
2024-09-09 17:50:50 -0400

    Added questions to the first lesson

---
+++ Exercises/Questions/Lesson 1 Exercise.py
@@ -0,0 +1,75 @@
+                                        ######################################################################
+                                        #  !                                Variables                        #
+                                        ######################################################################
+
+
+# Let's model real life scenarios with our exercises
+# Imagine we are building a web-site and we need to store user information
+#? BONUS POINTS - ask user to type the variable data all by themselves
+
+                                    # First of all we need to store the most basic information - email and password
+# 1. Create a variable that will store user email
+
+
+
+
+# 2. Create a variable that will store user password.  
+#? NOTE - in the real life scenarios we won't be storing password as a plain string. But for the purpose of the exercise lets imagine that it is a string
+
+
+
+                                    #  Okay, we are done with the basics. But what else can be stored as user data?
+# Find your usage scenario and create a fields that will have variables with types:
+
+
+#Integer
+
+
+
+# Float
+
+
+
+# Boolean
+
+
+
+# Create a constant variable (maybe there is a data that shouldn't be changed?)
+
+
+
+# Maybe some data won't have value yet. Let's create some variables-placeholders with None type
+
+
+                                        # Okay, we are done with creation, let's print the result!
+# Print all the data that you created earlier (the output can be on one or multiple lines)
+
+
+
+# Insert all the data in the text file and save it!
+
+
+
+                                                                #? CHALLENGE
+# This section covers topics that were not covered in the lesson, so you can challenge yourself finding solution to the real case scenarios by yourself
+# Be aware, that the tasks might be a little bit too hard for beginners 
+# Although I mentioned that we can save the data in the text file, there is also another way - by using Pickle framework
+# Save data in a file using Pickle
+
+
+# Load data from the file using Pickle and print it on the screen (with additional note, that it was retrieved by using Pickle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                #? READING CHALLENGE
+# Some programming languages use stack and heap concepts to store data in memory. But that is not the case of Python. Find out, how Python stores data in memory.
